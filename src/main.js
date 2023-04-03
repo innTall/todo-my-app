9226723a414db8f87457746cc11b3a5e87cad659
innTall <troll999@hot.ee>
2023-04-03 10:53:01 +0200

    base settings for frontend

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router/index.js';
 import './index.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia).use(router).mount('#app');
